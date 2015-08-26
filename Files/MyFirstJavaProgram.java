2b91d7dbbdf6bb1a07cb466e514cad6ea07e0046
James Loosli <loosli@gmail.com>
2015-08-26 12:43:53 -0700

    initial commit

---
+++ Files/MyFirstJavaProgram.java
@@ -0,0 +1,16 @@
+/**
+ * This is my first java program.  
+ * It will print 'Hello World' as the output
+ * @author Khoa Tran
+ */
+public class MyFirstJavaProgram {
+	
+	/**
+	 * Entry point to my program
+	 * @param args
+	 */
+	public static void main (String[] args)
+	{
+		System.out.println("Hello World");
+	}
+}
---
+++ Translation_Guide.md
@@ -0,0 +1,15 @@
+aw toe ma tic ollie = automatically
+fi = file
+in truck shun = instruction
+instet = instead
+it's a cute = execute
+languish = language
+masene = machine
+mi do = middle
+nat caam = next time
+obzek = object
+s s i = exercise
+syn tat = syntax   
+z = j
+zava = java
+zut = just
