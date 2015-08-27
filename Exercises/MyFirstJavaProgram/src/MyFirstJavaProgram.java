da4f713377bd140773b126bc3f9306ae7854fee9
James Loosli <loosli@gmail.com>
2015-08-26 20:29:13 -0700

    First demo project. I was supposed to do this in Eclipse

---
+++ Exercises/MyFirstJavaProgram/src/MyFirstJavaProgram.java
@@ -0,0 +1,15 @@
+/** 
+ * This is my first Java program.
+ * It will print "Hello World" as the output
+ * @author jloosli
+ *
+ */
+public class MyFirstJavaProgram {
+  /**
+   * Entry point to my program
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
